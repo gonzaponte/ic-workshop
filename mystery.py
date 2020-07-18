da00a77523c99347f4bb012dc5f7396445fb21cb
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2020-07-18 22:18:07 +0300

    .

---
+++ mystery.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 25 16:05:12 2017
+
+@author: alej
+"""
+
+def xxx(a,b):
+    if b < 0 or a < 0:
+        return 0
+    if a==b:
+        return 2
+    return 1
---
+++ mystery_test.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from pytest import mark
+from hypothesis import given, example
+from hypothesis.strategies import integers
+
+import mystery as mys
+
+
+@given(integers(), integers())
+@example(3, 0)
+def test_2(a, b):
+    assert mys.xxx(a, b) == mys.xxx(b, a)
+
+
+@given(integers(), integers(), integers())
+@example(3, 0, 1)
+def test_3(a, b, c):
+    assert mys.xxx(a, mys.xxx(b, c)) == mys.xxx(mys.xxx(a, b), c)
+    
+
+@given(integers())
+@example(3)
+def test_3(a):
+    assert mys.xxx(a, 0) == a
